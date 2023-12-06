521265aaf1a0f1fdbe6ff0f51ad7509daa72db2e
Subham Misra <subham.misra@snowflake.com>
2023-12-06 22:33:12 +0530

    adding commit logic

--- scripts/check-whitelist.js
+++ scripts/check-whitelist.js
@@ -6,7 +6,7 @@ const yaml = require('yaml');
 const whitelist = yaml.parse(fs.readFileSync('email-whitelist.yml', 'utf8'));
 const prAuthorEmail = process.env.PR_AUTHOR_EMAIL;
 
-const isWhitelisted = whitelist.domains.some(domain => prAuthorEmail.endsWith(`@${domain}`)) || whitelist.individual_accounts.includes(prAuthorEmail);
+const isWhitelisted = whitelist.domains.some(domain => prAuthorEmail.endsWith(`@${domain}`)) || whitelist.individual_accounts.some(account => account.email === prAuthorEmail);
 
 console.log(`Is PR author whitelisted? ${isWhitelisted}`);
 
